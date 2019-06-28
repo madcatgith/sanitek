e39a376a8a5a9cb89891b4339ce3a0a3aaca1d31
MaDCaT <wasd.exe@gmail.com>
2019-06-28 23:09:23 +0300

    still header|footer(

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,33 @@
 $( document ).ready(function() {
-    $('.viewed-products-slider').owlCarousel({
-        nav: true,
-        loop: true,
-        margin: 15,       
-        autoWidth: false,
-        dots: false,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    });
+	$('.viewed-products-slider').owlCarousel({
+		nav: true,
+		loop: true,
+		margin: 15,	
+		autoWidth: false,
+		dots: false,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	});
 });
+
+if ($(window).width() > 1023) {
+	$(window).scroll(function() {
+		//change offset to 150 on dev
+		if ($(this).scrollTop() >= 1) { 
+			if($("#small-header").hasClass("show") === false){
+				$("#small-header").addClass("show");
+			}
+		}else{
+			$("#small-header").removeClass("show");
+		}
+	});
+}
